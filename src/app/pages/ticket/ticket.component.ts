437910b69065b3bc360a9319b31fdfe66ace7153
AlonsoCod3 <codelonso@gmail.com>
2025-04-27 23:18:52 -0500

    Seccion hoy

--- src/app/pages/ticket/ticket.component.ts
+++ src/app/pages/ticket/ticket.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-ticket',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  @ViewChild("card")  tarjetaAfter!:ElementRef;
 
+  ngAfterViewInit() {
+  }
+  controlCard(){
+    this.tarjetaAfter.nativeElement.classList.contains("expand") ?
+    this.tarjetaAfter.nativeElement.classList.remove("expand") :
+    this.tarjetaAfter.nativeElement.classList.add("expand")
+  }
 }
