e007e3e6576647cb8091d9042a314d8bdc2c5ca3
AlonsoCod3 <codelonso@gmail.com>
2025-04-28 18:48:31 -0500

    estilos cambio de vista grid-list

--- src/app/pages/ticket/ticket.component.ts
+++ src/app/pages/ticket/ticket.component.ts
@@ -12,9 +12,26 @@ export class TicketComponent {
 
   ngAfterViewInit() {
   }
-  controlCard(){
-    this.tarjetaAfter.nativeElement.classList.contains("expand") ?
-    this.tarjetaAfter.nativeElement.classList.remove("expand") :
-    this.tarjetaAfter.nativeElement.classList.add("expand")
+  controlCard(card:any){
+    card.classList.contains("expand") ?
+    card.classList.remove("expand") :
+    card.classList.add("expand")
+  }
+  changeData(type:string, card:any) {
+    if(type == "grid") {
+      // this.tarjetaAfter.nativeElement.childNodes[0].classList.remove("c-height")
+      // this.tarjetaAfter.nativeElement.classList.remove("expand")
+      // this.tarjetaAfter.nativeElement.childNodes[0].classList.add("c-width")
+      card.childNodes[0].classList.remove("c-height")
+      card.classList.remove("expand")
+      card.childNodes[0].classList.add("c-width")
+    }
+    if(type == "list") {
+      // this.tarjetaAfter.nativeElement.childNodes[0].classList.remove("c-width")
+      // this.tarjetaAfter.nativeElement.classList.add("expand")
+      // this.tarjetaAfter.nativeElement.childNodes[0].classList.add("c-height")
+      card.childNodes[0].classList.remove("c-width")
+      card.childNodes[0].classList.add("c-height")
+    }
   }
 }
