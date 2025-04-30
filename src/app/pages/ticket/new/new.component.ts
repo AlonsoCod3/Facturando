ae1cc45e786c1a5309e3dada24461b4d92c3603a
AlonsoCod3 <codelonso@gmail.com>
2025-04-29 22:02:53 -0500

    Añadido router y funcion inicial de añadir items

--- src/app/pages/ticket/new/new.component.ts
+++ src/app/pages/ticket/new/new.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './new.component.html',
   styleUrl: './new.component.css'
 })
 export class NewComponent {
+
+  addItem(){
+    console.log("Añadi unitem")
+  }
 
 }
