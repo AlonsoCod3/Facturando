a0a19636e84d034f599b22cffb85c6194534ec3f
AlonsoCod3 <codelonso@gmail.com>
2025-05-01 22:43:15 -0500

    Funcion de busqueda de documento

--- src/app/pages/ticket/new/new.component.ts
+++ src/app/pages/ticket/new/new.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,9 +8,28 @@ import { RouterLink } from '@angular/router';
   styleUrl: './new.component.css'
 })
 export class NewComponent {
+  @ViewChild("dialog") dialog?:ElementRef
+
+  constructor(){}
 
   addItem(){
     console.log("Añadi unitem")
+    this.dialog?.nativeElement.setAttribute("open", "")
   }
-
+  addFavorite() {
+    console.log("añadido")
+  }
+  onInputDocument(event:any){
+    if ( event.length === 8 ) {
+      // this.searchData(event)
+      console.log("buscando DNI")
+    }
+  }
+  searchData(value:string) {
+    // this.consultDNI(value).subscribe(
+    //   (x) => {
+    //     console.log(x)
+    //   }
+    // )
+  }
 }
