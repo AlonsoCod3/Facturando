1c43e5eeb89fefc49b200170d2a0385281ef098d
AlonsoCod3 <codelonso@gmail.com>
2025-05-04 11:35:45 -0500

    Boton de cantidad de productos y funcionalidad de precios

--- src/app/pages/ticket/new/new.component.ts
+++ src/app/pages/ticket/new/new.component.ts
@@ -2,27 +2,48 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ClientService } from '../../../core/services/client.service';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { DecimalPipe   } from '@angular/common';
 
 @Component({
   selector: 'app-new',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, DecimalPipe  ],
   templateUrl: './new.component.html',
   styleUrl: './new.component.css'
 })
 export class NewComponent {
+  @ViewChild("dialog") dialog!:ElementRef
   @ViewChild("client_name") name!:ElementRef
   @ViewChild("client_address") address!:ElementRef
 
-  variantes:any
-  products:any
+  client = {}
+  variantes:any = {}
+  products = [
+    {
+      id: 0,
+      nombre: "Ceviche",
+      precio: "22",
+      cantidad: 1,
+      total: 22
+    }
+  ]
+  igv = new FormControl(10/100)
+
+  totalAmount:number = 0
+
 
   p_name:FormControl = new FormControl("")
   p_price:FormControl = new FormControl("")
-  p_description:FormControl = new FormControl("")
+  p_amount:FormControl = new FormControl(1)
 
   constructor(
     private clientService:ClientService
-  ){}
+  ){
+    this.sumTotals()
+  }
+
+  sumTotals() {
+    this.totalAmount = this.products.map(x => x.total).reduce((acc, current) => acc + current, 0)
+  }
 
   firstLetter(event:any){
     let inputValue = event.target.value;
@@ -31,17 +52,28 @@ export class NewComponent {
   numberPress(e:any){
     return /[0-9]/i.test(e.key)
   }
-
-  client = {}
+  subtractAmount(){
+    if(Number(this.p_amount.value) != 0){
+      this.p_amount.setValue(Number(this.p_amount.value) - 1);
+    }
+    
+  }
+  addAmount(){
+    this.p_amount.setValue(Number(this.p_amount.value) + 1);
+  }
 
   addItem(){
     console.log("Añadi unitem")
     let product = {
+      id: this.products.length,
       nombre:this.p_name.value,
       precio:this.p_price.value,
-      descripcion:this.p_description.value
+      cantidad:this.p_amount.value,
+      total: this.p_price.value * this.p_amount.value
     }
-    console.log(product)
+    this.products.push(product)
+    this.sumTotals()
+    this.dialog.nativeElement.close()
   }
 
   addFavorite() {
