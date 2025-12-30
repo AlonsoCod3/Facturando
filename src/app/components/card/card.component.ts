2807c73753814c00324096c66ff0b9b042725679
AlonsoCod3 <codelonso@gmail.com>
2025-12-30 18:30:49 +0000

    Function of select and add new cards on HomePage

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,13 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { JsonPipe, NgClass } from '@angular/common';
+import { Component, ElementRef, EventEmitter, Input, Output, OnInit, ViewChild, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent implements OnInit{
+
   @Input("typo") typo : string = ""
 
-  ngOnInit(): void {}
+  // Data only of card selectable
+  @Input("typo-data") tData : {value:string, price:string,id:string,select:boolean}
+  @Output("arrayClickable") arrayClickable: EventEmitter<any> = new EventEmitter();
+  @Output("arrayUnclickable") arrayUnclickable: EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+  clickable(value,card,type="input"):void{
+    type == "card" ? value.checked = !value.checked : null
+    value.checked ? (card.select = true, this.getEmit(card,this.arrayClickable)) : (card.select = false, this.getEmit(card,this.arrayUnclickable))
+  }
+  getEmit(value, cost){
+    cost.emit(value)
+  }
+
+
 }
