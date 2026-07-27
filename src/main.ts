ec5666e9f3912d162987866c6e6d2725738ff5fa
AlonsoCod3 <codelonso@gmail.com>
2026-07-27 04:17:37 +0000

    add loader in init page

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,42 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { Component } from '@angular/core';
-import { RouterModule, provideRouter, withComponentInputBinding } from '@angular/router';
+import { ResolveEnd, ResolveStart, Router, RouterModule, provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import routes from './routes';
+import { LoaderService } from './app/core/services/loader.service';
+import { LoaderComponent } from './app/components/loader/loader.component';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule],
-  template: `<router-outlet />`,
+  imports: [RouterModule, LoaderComponent],
+  template: `
+  @if(loader.loading()){
+  <app-loader />
+  }
+  <router-outlet />
+  `,
 })
-export class App {}
+export class App {
+
+  constructor(private router: Router,public loader: LoaderService) {
+    this.router.events.subscribe(event => {
+      if (event instanceof ResolveStart) {
+        this.loader.show();
+      }
+
+      if (event instanceof ResolveEnd) {
+        this.loader.hide();
+      }
+    });
+  }
+
+}
 
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(),
+    LoaderService
   ],
 });
