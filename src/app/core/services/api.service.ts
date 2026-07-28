b24dbf2c875310195ed387c4a3fc583d4e992bb4
AlonsoCod3 <codelonso@gmail.com>
2026-07-28 16:47:02 +0000

    centralizate forkjoin apis in initialLoader

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,33 +1,50 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap} from 'rxjs';
+import { Observable, forkJoin, of, timer} from 'rxjs';
+import { catchError, map, retry} from 'rxjs/operators';
+import { cacheWithTTL } from '../utils/cache-with-ttl.util';
+import { ProductService } from './product.service';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
   private http = inject(HttpClient);
+  private API_RENIEC = import.meta.env.NG_APP_URL + "/verify"
+  private productService = inject(ProductService);
+  
+  private checkCache = cacheWithTTL(
+    () => this.http.get(this.API_RENIEC)
+    .pipe(
+      map((response:any)=> {
+        // si la API responde
+        if(response.status){
+          console.log("Estado de API reniec:", response.status)
+          return true
+        }
+        throw new Error('API aún no está lista')
+      }),
+      retry({
+        count: 10,
+        delay: (error, retryCount) => timer(3000),
+        resetOnSuccess: true,
+      }), catchError((error)=>{
+        console.log('Se agotaron los reintentos, la API no respondió a tiempo');
+        return of(false);
+      }),
 
-  private dataCache = signal<any[] | null>(null);
+    ),
+    300000
+  )
 
-  private API_URL = import.meta.env.NG_APP_PRODUCT
 
-  getData() {
-    if (this.dataCache()) {
-      return new Observable(observer => {
-        observer.next(this.dataCache()!);
-        observer.complete();
-      });
-    }
-
-    return this.http.get<any[]>(this.API_URL + "/verify").pipe(
-      tap(posts => this.dataCache.set(posts))
-    );
+  initializeApp(): Observable<any> {
+    return forkJoin({
+      reniec: this.check(),
+      products: this.productService.check(),
+    })
   }
 
-  setCache(data: any) {
-    this.dataCache.set(data);
+  check(): Observable<boolean> {
+    return this.checkCache.get();
   }
 
-  clearCache() {
-    this.dataCache.set(null);
-  }
 }
