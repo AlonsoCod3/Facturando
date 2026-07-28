8fb4c56d4fdb28d7264a53d38f66876b6d2e04ed
AlonsoCod3 <codelonso@gmail.com>
2026-07-28 16:40:30 +0000

    add shareReplay in cahingService

--- src/app/core/services/caching.service.ts
+++ src/app/core/services/caching.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CachingService {
-
-  private cache = new Map<string, any>()
-
-  constructor() { }
+  private cache = new Map<string, Observable<any>>()
 
   get(key:string):Observable<any> |null {
-    return this.cache.get(key) ? of(this.cache.get(key)) : null
+    return this.cache.get(key) ?? null
   }
 
   set(key:string , value:any):void{
     this.cache.set(key,value)
+  }
+
+  clear(key:string):void{
+    this.cache.delete(key)
   }
 }
