b3b1c50dc2aa53035921644b14422135d953f15e
AlonsoCod3 <codelonso@gmail.com>
2026-07-28 16:36:24 +0000

    new funcionality with share request in productService

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,67 +1,84 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { CachingService } from './caching.service';
-import { Observable, catchError, map, tap, throwError } from 'rxjs';
+import { Observable, shareReplay, tap, throwError, timer, of } from 'rxjs';
+import { catchError, map, retry} from 'rxjs/operators';
+import { cacheWithTTL } from '../utils/cache-with-ttl.util';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ProductService {
-  private pathProducts = import.meta.env.NG_APP_PRODUCT
-
   private http = inject(HttpClient)
+  private pathProducts = import.meta.env.NG_APP_PRODUCT
   private cacheService = inject(CachingService)
-  private dataCache = signal<any[] | null>(null);
+  private checkCache = cacheWithTTL(
+    () => this.http.get(`${this.pathProducts}/verify`)
+      .pipe(
+            map((response:any)=> {
+              // si la API responde
+              if(response){
+                console.log("Estado de API Products:", response)
+                return true
+              }
+              throw new Error('API aún no está lista')
+            }),
+            retry({
+              count: 10,
+              delay: (error, retryCount) => timer(3000),
+              resetOnSuccess: true,
+            }), catchError((error)=>{
+              console.log('Se agotaron los reintentos, la API no respondió a tiempo');
+              return of(false);
+            }),
+          ),
+    300000
+  )
 
   constructor() { }
 
   public getProducts():Observable<any> {
     const url = `${this.pathProducts}/products/`
-    const cachedResponse = this.cacheService.get(url);
-    if (cachedResponse){
-      return cachedResponse
-    }else {
-      return this.http.get(url)
-      .pipe(
-        tap(response => this.cacheService.set(url,response)),
-        map(response => response),
-        catchError(requestError=>throwError(requestError))
-      )
+    const cached = this.cacheService.get(url);
+    
+    if (cached){
+      console.log("respuesta cacheada")
+      return cached
     }
+    console.log('No hay caché, creando la petición HTTP.');
+    const request$ =  
+    this.http.get(url)
+    .pipe(
+      tap({
+        subscribe: () => console.log('Alguien se suscribió, la petición se está disparando ahora'),
+        next: (response) => console.log('Respuesta recibida de la API:', response),
+        error: (err) => console.log('Error en la petición:', err),
+      }),
+        map(response => response),
+        catchError(requestError=> {
+          this.cacheService.clear(url)
+          return throwError(() => requestError)
+        }),
+        shareReplay(1)
+      )
+      this.cacheService.set(url, request$)
+      console.log('Observable guardado en caché (aún sin ejecutar)');
+      return request$
   }
+
   public newProduct(body:any):Observable<any> {
     const url = `${this.pathProducts}/products/`
-    // const cachedResponse = this.cacheService.get(url);
-    // if (cachedResponse){
-    //   return cachedResponse
-    // }else {
+
       return this.http.post(url, body)
       .pipe(
-        // tap(response => this.cacheService.set(url,response)),
         map(response => response),
         catchError(requestError=>throwError(requestError))
       )
-    // }
   }
 
-  getData() {
-    if (this.dataCache()) {
-      return new Observable(observer => {
-        observer.next(this.dataCache()!);
-        observer.complete();
-      });
-    }
-
-    return this.http.get<any[]>(this.pathProducts + "/verify").pipe(
-      tap(posts => this.dataCache.set(posts))
-    );
+  check(): Observable<boolean> {
+    return this.checkCache.get();
   }
 
-  setCache(data: any) {
-    this.dataCache.set(data);
-  }
-
-  clearCache() {
-    this.dataCache.set(null);
+  refreshProducts(): void {
+    this.checkCache.clear();
   }
 }
