9069f5854e66f0634d2732e59bbd559928ffee66
AlonsoCod3 <codelonso@gmail.com>
2026-08-04 09:00:44 -0500

    Nueva card especifica de customer y loader

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,7 @@
 import { NgClass } from '@angular/common';
-import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, inject } from '@angular/core';
+import { NgxSkeletonLoaderComponent } from 'ngx-skeleton-loader';
+import { CustomerService } from '../../core/services/customer.service';
 
 interface CardData {
   id?: string;
@@ -8,15 +10,19 @@ interface CardData {
   value?: string;
   price?: string;
   docNumber?: string;
+  docType?: string;
+  phone?: string;
 }
 
 @Component({
   selector: 'app-card',
-  imports: [NgClass],
+  imports: [NgClass, NgxSkeletonLoaderComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent implements OnInit{
+
+  customerService = inject(CustomerService)
 
   @Input("typo") typo : string = ""
 
@@ -26,15 +32,40 @@ export class CardComponent implements OnInit{
   @Output("arrayClickable") arrayClickable: EventEmitter<any> = new EventEmitter();
   @Output("arrayUnclickable") arrayUnclickable: EventEmitter<any> = new EventEmitter();
 
+  customerOptions:boolean = false
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  // daily
   clickable(value:any,card:any,type="input"):void{
     type == "card" ? value.checked = !value.checked : null
     value.checked ? (card.select = true, this.getEmit(card,this.arrayClickable)) : (card.select = false, this.getEmit(card,this.arrayUnclickable))
   }
+  // Customer Options
+  clickOptions(value:any,card:any):void{
+    value.checked = !value.checked
+    this.customerOptions = value.checked
+  }
+  clickEdit(){
+    this.customerService.options({
+      type: "edit",
+      data: this.tData
+    })
+  }
+  clickDelete(){
+    this.customerService.options({
+      type: "delete",
+      data: this.tData
+    })
+  }
+  clickInvoice(){
+    this.customerService.options({
+      type: "invoice",
+      data: this.tData
+    })
+  }
+
   getEmit(value:any, cost:any){
     cost.emit(value)
   }
