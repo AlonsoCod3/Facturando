f7644ac18b3367deea138a1c087a6d852c0f4e9e
AlonsoCod3 <codelonso@gmail.com>
2026-08-05 13:05:39 -0500

    nuevo diseño para pagina error

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { inject } from '@angular/core';
+import { inject ,signal } from '@angular/core';
 import { map } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
 
@@ -14,6 +14,14 @@ import { toSignal } from '@angular/core/rxjs-interop';
 export class NotFoundComponent {
 
   private route = inject(ActivatedRoute)
+  returnUrl = signal<string>('/');
+  isReloading = signal(false);
+
+  constructor(){
+    this.route.queryParams.subscribe(params => {
+      this.returnUrl.set(params['returnUrl'] || '/');
+    });
+  }
 
   errorType = toSignal(
     this.route.queryParams.pipe(map(params => params['type']))
@@ -22,4 +30,10 @@ export class NotFoundComponent {
   errorCode = toSignal(
     this.route.queryParams.pipe(map(params => params['code']))
   )
+
+  reintentar() {
+    // Recarga completa: reinicia toda la app, incluyendo cachés en memoria
+    this.isReloading.set(true);
+    window.location.href = this.returnUrl();
+  }
 }
